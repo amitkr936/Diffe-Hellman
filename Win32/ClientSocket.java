4b29426153898ee960e70b05f4bb17486e623531
amitkr936 <amitkrishna936@gmail.com>
2018-10-28 13:14:46 +0530

    First

---
+++ Win32/ClientSocket.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSocket 
+{
+//Server
+	public static void main(String arge[]) throws IOException
+	{
+		Socket ss = null;
+		int number = 0;
+		try {
+			ServerSocket s1=new ServerSocket(1342);
+			ss=s1.accept();
+			Scanner sc=new Scanner(ss.getInputStream());
+			number=sc.nextInt();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int temp=number*2;//modify the ip using dh
+		PrintStream p=new PrintStream(ss.getOutputStream());
+		p.println(temp);
+		
+	}
+}
---
+++ Win32/Server_Socket.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Server_Socket
+{//Client
+	public static void main(String args[]) throws UnknownHostException, IOException
+	{
+		Scanner sc=new Scanner(System.in);
+		Socket s=new Socket("127.0.0.1",1342);//Specify the IP address and Port number
+		//accept from server
+		Scanner sc1=new Scanner(s.getInputStream());
+		System.out.println("Enter any Number");
+		int number=sc.nextInt();
+		PrintStream p =new PrintStream(s.getOutputStream());
+		p.println(number);
+	    int temp=sc1.nextInt();
+		System.out.println(temp);
+	}
+}
